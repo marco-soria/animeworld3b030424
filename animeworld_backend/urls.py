540ec8f62e0cad31058af358a289ba199d9b71b1
marco-soria <marco.soria@pucp.edu.pe>
2024-03-15 09:21:09 -0500

    urls global

--- animeworld_backend/urls.py
+++ animeworld_backend/urls.py
@@ -41,7 +41,10 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
+    path('api/',include('api.urls')),
+    path('login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
