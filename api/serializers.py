050c90828e71d5c93400e38c473fe25e1b13f83b
marco-soria <marco.soria@pucp.edu.pe>
2024-03-26 15:12:00 -0500

    added serializers

--- api/serializers.py
+++ api/serializers.py
@@ -23,7 +23,7 @@ class ReviewSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_subscribed', 'date_joined']
+        fields = '__all__'
 
 # Serializer para OrderItem
 class OrderItemSerializer(serializers.ModelSerializer):
@@ -46,3 +46,54 @@ class OrderSerializer(serializers.ModelSerializer):
         model = Order
         fields = ['id', 'user', 'total_price', 'is_delivered', 'delivered_at', 'created_at', 'items', 'shipping_address']
 
+
+#user
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        refresh = RefreshToken.for_user(user)
+        return refresh
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        refresh = self.get_token(self.user)
+        data['refresh'] = str(refresh)
+        data['access'] = str(refresh.access_token)
+        data['email'] = self.user.email
+        data['username'] = self.user.username
+        return data
+
+class RegisterSerializer(serializers.ModelSerializer):
+    # Define fields for the serializer, including password
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+
+    class Meta:
+        # Specify the model that this serializer is associated with
+        model = User
+        # Define the fields from the model that should be included in the serializer
+        fields = ('first_name', 'last_name', 'email', 'password')
+
+    def validate(self, attrs):
+        # Return the validated attributes
+        return attrs
+
+    def create(self, validated_data):
+        # Define a method to create a new user based on validated data
+        user = User.objects.create_user(
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+
+        # Set the user's password based on the validated data
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
+    
+class PasswordResetSerializer(serializers.Serializer):
+    email = serializers.EmailField()
